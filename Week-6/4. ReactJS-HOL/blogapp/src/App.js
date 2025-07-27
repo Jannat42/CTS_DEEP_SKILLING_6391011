4fa65ef34bca9068690d6dec2251a8bc18b8ad18
Jannat42 <thevisualizm@gmail.com>
2025-07-27 22:51:40 +0530

    Updated Week-6

---
+++ Week-6/4. ReactJS-HOL/blogapp/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Posts from './Posts/Posts';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Blog App</h1>
+      <Posts/>
+    </div>
+  );
+}
+
+export default App;
